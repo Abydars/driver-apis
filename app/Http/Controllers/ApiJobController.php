08186ba69f06a9ca6c9977abccd507c964f87fa8
AbydArs <abidr.w@gmail.com>
2017-12-19 22:36:12 +0500

    job update event

--- app/Http/Controllers/ApiJobController.php
+++ app/Http/Controllers/ApiJobController.php
@@ -98,6 +98,8 @@ class ApiJobController extends Controller
 			$job->status = 'done';
 
 			if ( $job->save() ) {
+				event( new NewEntryEvent( 'job_updated', $job, $job->user->id, $job->passenger->id ) );
+
 				return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), null, __( 'strings.job.update_success' ) );
 			}
 		}
